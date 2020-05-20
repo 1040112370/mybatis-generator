8a46c833c24c97e879c7fdcb0ee95a103bcc3197
cuikang <337946312@qq.com>
2020-05-20 18:17:33 +0800

    新建工程

---
+++ src/main/java/com/baomidou/ant/test/controller/AreaController.java
@@ -0,0 +1,20 @@
+package com.baomidou.ant.test.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 省市区表 前端控制器
+ * </p>
+ *
+ * @author jobob
+ * @since 2020-05-20
+ */
+@RestController
+@RequestMapping("/test/area")
+public class AreaController {
+
+}
---
+++ src/main/java/com/baomidou/ant/test/mapper/AreaMapper.java
@@ -0,0 +1,16 @@
+package com.baomidou.ant.test.mapper;
+
+import com.baomidou.ant.test.entity.Area;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 省市区表 Mapper 接口
+ * </p>
+ *
+ * @author jobob
+ * @since 2020-05-20
+ */
+public interface AreaMapper extends BaseMapper<Area> {
+
+}
